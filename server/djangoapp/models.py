13368b9c98aa6ab82343369c770c9f2c2d73571b
Amani Jammoul <amani.jammoul@mail.mcgill.ca>
2024-04-14 18:26:22 -0400

    CarMake & CarModel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,12 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name 
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +29,32 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('Sedan', 'Sedan'),
+        ('Suv', 'Suv'),
+        ('Wagon', 'Wagon'),
+    ]
+    type = models.CharField(choices=CAR_TYPES, default='Sedan', max_length=100)
+    year = models.IntegerField(default=2015, 
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    ENGINE_TYPE = [
+        ('Gasoline', 'Gasoline'),
+        ('Diesel', 'Diesel'),
+        ('Hybrid', 'Hybrid'),
+        ('Electric', 'Electric'),
+    ]
+    engine_type = models.CharField(choices=ENGINE_TYPE, default='Gasoline', max_length=50)
+    TRANS_TYPE = [
+        ('Automatic', 'Automatic'),
+        ('Manual', 'Manual'),
+    ]
+    transmission_type = models.CharField(choices=TRANS_TYPE, default='Automatic', max_length=50)
+    horsepower = models.PositiveIntegerField(default=220)
+    
+
+    def __str__(self):
+        return f"{self.make.name} {self.name} ({self.year})"
+
