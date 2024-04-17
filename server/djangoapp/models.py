3133ccd64a227ad556093a31c3712860809deb0a
Amani Jammoul <amani.jammoul@mail.mcgill.ca>
2024-04-16 23:23:13 -0400

    black reformatting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,11 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -17,7 +17,7 @@ class CarMake(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.name 
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -33,28 +33,31 @@ class CarModel(models.Model):
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('Sedan', 'Sedan'),
-        ('Suv', 'Suv'),
-        ('Wagon', 'Wagon'),
+        ("Sedan", "Sedan"),
+        ("Suv", "Suv"),
+        ("Wagon", "Wagon"),
     ]
-    type = models.CharField(choices=CAR_TYPES, default='Sedan', max_length=100)
-    year = models.IntegerField(default=2015, 
-        validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    type = models.CharField(choices=CAR_TYPES, default="Sedan", max_length=100)
+    year = models.IntegerField(
+        default=2015, validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+    )
     ENGINE_TYPE = [
-        ('Gasoline', 'Gasoline'),
-        ('Diesel', 'Diesel'),
-        ('Hybrid', 'Hybrid'),
-        ('Electric', 'Electric'),
+        ("Gasoline", "Gasoline"),
+        ("Diesel", "Diesel"),
+        ("Hybrid", "Hybrid"),
+        ("Electric", "Electric"),
     ]
-    engine_type = models.CharField(choices=ENGINE_TYPE, default='Gasoline', max_length=50)
+    engine_type = models.CharField(
+        choices=ENGINE_TYPE, default="Gasoline", max_length=50
+    )
     TRANS_TYPE = [
-        ('Automatic', 'Automatic'),
-        ('Manual', 'Manual'),
+        ("Automatic", "Automatic"),
+        ("Manual", "Manual"),
     ]
-    transmission_type = models.CharField(choices=TRANS_TYPE, default='Automatic', max_length=50)
+    transmission_type = models.CharField(
+        choices=TRANS_TYPE, default="Automatic", max_length=50
+    )
     horsepower = models.PositiveIntegerField(default=220)
-    
 
     def __str__(self):
         return f"{self.make.name} {self.name} ({self.year})"
-
